38f19310d99480691bf9c3279bcc9bb40cb05b4e
SzhenBBK <edsoneto.50@gmail.com>
2021-04-27 15:07:08 -0300

    Update price LP to V2

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xfc8e68e30350c6603d3d29fcc8e676380c28fcf4',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x6154a783E73Ac4A9C8fcb015C36BcE6CE1Ee6D1e'
+  lpAddress: '0xeE56e52923B4d4d15cfE14F281BbA471416C534d'
 }
 
 export default priceContracts
